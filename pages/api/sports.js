43111dc3b31a972fb7e1aea7ebfb86974a955c71
Nikil192 <nikil19.ve@gmail.com>
2026-05-15 16:37:52 +0530

    extend week range to 10 days to catch F1 weekends

--- pages/api/sports.js
+++ pages/api/sports.js
@@ -43,7 +43,7 @@ function getWeekRange() {
   const start = new Date();
   start.setHours(0, 0, 0, 0);
   const end = new Date(start);
-  end.setDate(end.getDate() + 7);
+  end.setDate(end.getDate() + 12);
   return { start, end };
 }
 
